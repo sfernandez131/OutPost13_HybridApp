4aed98096dadafe705e66a19c98be07d0960f19d
Scott Fernandez <sfernandez@goldstarfinancial.com>
2021-07-14 11:16:26 -0400

    Add project files.

---
+++ OutPost13_HybridApp.macOS/Main.cs
@@ -0,0 +1,16 @@
+﻿using AppKit;
+using Microsoft.MobileBlazorBindings.WebView.macOS;
+
+namespace OutPost13_HybridApp.macOS
+{
+    internal static class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            BlazorHybridMacOS.Init();
+            NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new AppDelegate();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ OutPost13_HybridApp/CounterState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OutPost13_HybridApp
+{
+    internal class CounterState
+    {
+        public int CurrentCount { get; private set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+            StateChanged?.Invoke();
+        }
+
+        public event Action StateChanged;
+    }
+}
